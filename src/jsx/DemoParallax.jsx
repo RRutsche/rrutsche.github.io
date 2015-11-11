9c5445ddc66a73a3fba784cb8ca81e226045988f
Richard Rutsche <richardrutsche@gmail.com>
2015-11-12 00:31:42 +0100

    more space for the parallax

--- src/jsx/DemoParallax.jsx
+++ src/jsx/DemoParallax.jsx
@@ -33,8 +33,11 @@ var DemoParallax = React.createClass({
 				</div>
 				<Parallax bgImage="www/assets/1.jpg" strength={400} log={true}>
 					<br/>
+					<br/>
+					<br/>
 					<h1 style={fontStyle}>...</h1>
 					<h1 style={fontStyle2}>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops</h1>
+					<br/>
 					<br/>
 				</Parallax>
 				<div>
@@ -43,12 +46,17 @@ var DemoParallax = React.createClass({
 					<br/>
 				</div>
 				<Parallax strength={300}>
+					<br />
+					<br />
 					<Background>
 						<img src="http://www.fillmurray.com/400/300" alt="fill murray"/>
 						<div style={{width: 800, height: 300, backgroundColor: '#450093'}}></div>
 						<img src="http://www.fillmurray.com/500/300" alt="fill murray"/>
 					</Background>
+					<br />
 					<h1 style={fontStyle2}>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops</h1>
+					<br />
+					<br />
 				</Parallax>
 
 				<div>
--- www/js/app.built.js
+++ www/js/app.built.js
@@ -31919,6 +31919,8 @@ var DemoParallax = React.createClass({
 				Parallax,
 				{ bgImage: 'www/assets/1.jpg', strength: 400, log: true },
 				React.createElement('br', null),
+				React.createElement('br', null),
+				React.createElement('br', null),
 				React.createElement(
 					'h1',
 					{ style: fontStyle },
@@ -31929,6 +31931,7 @@ var DemoParallax = React.createClass({
 					{ style: fontStyle2 },
 					'A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops'
 				),
+				React.createElement('br', null),
 				React.createElement('br', null)
 			),
 			React.createElement(
@@ -31945,6 +31948,8 @@ var DemoParallax = React.createClass({
 			React.createElement(
 				Parallax,
 				{ strength: 300 },
+				React.createElement('br', null),
+				React.createElement('br', null),
 				React.createElement(
 					Background,
 					null,
@@ -31952,11 +31957,14 @@ var DemoParallax = React.createClass({
 					React.createElement('div', { style: { width: 800, height: 300, backgroundColor: '#450093' } }),
 					React.createElement('img', { src: 'http://www.fillmurray.com/500/300', alt: 'fill murray' })
 				),
+				React.createElement('br', null),
 				React.createElement(
 					'h1',
 					{ style: fontStyle2 },
 					'A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops'
-				)
+				),
+				React.createElement('br', null),
+				React.createElement('br', null)
 			),
 			React.createElement(
 				'div',
